7430b4b6a9656ca35cd89405df5747b20277d45c
T. E. Pickering <te.pickering@gmail.com>
2019-09-16 17:33:03 -0700

    add initial cut at making containerized vnc desktop with iraf

---
+++ centos/install/tools.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+echo "Install some common tools for further installation"
+yum -y install epel-release 
+yum -y update
+yum -y install vim sudo wget which net-tools bzip2 telnet numpy #used for websockify/novnc
+yum clean all
