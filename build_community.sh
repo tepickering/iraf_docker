80f0de4d90aad3e6bf9f6ed16f96d005e38b026d
T. E. Pickering <te.pickering@gmail.com>
2019-10-09 01:20:03 +0200

    flesh out dockerfiles and scripts for running iraf via the debian/ubuntu packages

---
+++ build_community.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# build incantation to set up permissions within container to match those of the user who builds it.
+# this is to facilitate writing of outputs to host directories.
+docker build --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER}) -t iraf-community . -f Dockerfile-community
